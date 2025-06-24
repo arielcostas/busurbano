98203907c2a0d7ab5c7111e6e659b7f0179ccadf
Ariel Costas Guerrero <ariel@costas.dev>
2025-06-24 15:03:47 +0200

    Fix build

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from "@react-router/dev/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
