5d1309216cec36d152a2c0788dc846e784c75497
Ariel Costas Guerrero <ariel@costas.dev>
2025-10-21 19:44:48 +0200

    update action, backend deploymemt

--- src/frontend/public/pwa-worker.js
+++ src/frontend/public/pwa-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "2025-1013a";
+const CACHE_VERSION = "2025-1021a";
 const STATIC_CACHE_NAME = `static-cache-${CACHE_VERSION}`;
 const STATIC_CACHE_ASSETS = [
   "/favicon.ico",
